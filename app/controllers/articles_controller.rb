cafae8db1c25943302832b1ee9cd269286adfbb5
CaseyInHaengsin <casey.rowley89@gmail.com>
2020-03-11 07:25:54 -0600

    added bootstrap via webpack.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,7 +28,6 @@ end
 
 def show
  
-
 end
 
 def update
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,14 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import "./custom";
+import "bootstrap";
+import "../stylesheets/application";
+
+
+document.addEventListener("turbolinks:load", () => {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,5 +23,3 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-//= require jquery
-//= require bootstrap-sprockets
