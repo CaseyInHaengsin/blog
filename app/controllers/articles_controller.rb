88c4918c26eefcedc44669752c02b892281d168f
CaseyInHaengsin <casey.rowley89@gmail.com>
2020-03-17 04:47:09 -0600

    added association to user.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,7 @@ end
 def create
     
     @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
         flash[:notice] = "article was successfully created"
         redirect_to article_path(@article)
@@ -31,7 +32,6 @@ def show
 end
 
 def update
-    
 
     if @article.update(article_params)
         flash[:notice] = "article was successfully updated"
