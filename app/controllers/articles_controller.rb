5b71d10a24ec2a748a08ff470afdbaa93cb0c143
CaseyInHaengsin <casey.rowley89@gmail.com>
2020-03-18 07:51:35 -0600

    fixed hard-coded user issue.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,7 +20,7 @@ end
 def create
     
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
         flash[:notice] = "article was successfully created"
         redirect_to article_path(@article)
