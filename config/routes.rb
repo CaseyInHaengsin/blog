9471af65fb47117fccde4c015bfb45ed89d20a87
CaseyInHaengsin <casey.rowley89@gmail.com>
2020-03-24 06:34:23 -0600

    adding API for blog.. for fun.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :users do
+        resources :user
+      end
+      
+    end
+  end
 
+  
   root 'home#home'
 
   get 'about', to: 'home#about'
